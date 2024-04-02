36828af795685740826f419ed94956c19558f6da
Aravind Chinna <151151715+aravi143@users.noreply.github.com>
2024-04-02 15:14:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let charCount = {}; // Object to store character counts
+    let result = null;
+    
+    // Count occurrences of each character
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+        charCount[char] = (charCount[char] || 0) + 1;
+    }
+
+    // Find the first non-repeated character
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+        if (charCount[char] === 1) {
+            result = char;
+            break;
+        }
+    }
+
+    return result;
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
